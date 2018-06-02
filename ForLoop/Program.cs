74a1612d0299e6024171e8373d10bf5a588e644f
Unknown <deyi@raven.com>
2018-06-02 18:06:43 -0500

    Added class notes

---
+++ ForLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 52)
+                {
+                    continue;
+                }
+                int eachElement = i + 1;
+                Console.Write(eachElement + " ");
+                sum = sum + eachElement;
+            }
+            Console.WriteLine("The sum is: " + sum);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < i+1; j++)
+                {
+                    Console.Write(j+1);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ForeachLoop/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //immutable == not able to change
+            int[] anotherArray = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int[] intArray = new int[5];
+            intArray[0] = 1;
+            intArray[1] = 2;
+            intArray[2] = 3;
+            intArray[3] = 4;
+            intArray[4] = 5;
+            //Example of an array not being able to change.
+            //intArray[5] = 6;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine(intArray[i]);
+            }
+
+            List<int> convertedList = intArray.ToList();
+
+            foreach (int anInt in intArray)
+            {
+                Console.WriteLine(anInt);
+            }
+            Console.WriteLine("--------------------------");
+            List<int> intList = new List<int>();
+            intList.Add(6);
+            intList.Add(6);
+            intList.Add(8);
+            intList.Add(9);
+            intList.AddRange(intArray);
+            intList.Remove(6);
+
+            int[] convertedArray = intList.ToArray();
+
+            foreach (int anInt in intList)
+            {
+                Console.WriteLine(anInt);
+            }
+        }
+    }
+}
