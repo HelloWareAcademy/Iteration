2ab5d76040fc396ad15841ca13fb9c04592b65f1
Xin Zhang <xzhanguh@mail.com>
2018-06-14 21:40:52 -0500

    add homework

---
+++ Crystal/Pattern1_Xin/Pattern1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write(i + 1);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
